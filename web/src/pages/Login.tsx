a8f13be345fb9d93ce3007b8d635cb4c6a47a404
beatrizmakowski <bemakow@gmail.com>
2022-06-19 22:35:53 -0300

    feat: adjusting font sizes

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -2,32 +2,56 @@ import {
     Box,
     Button,
     Container,
-    Grow,
     Paper,
     Stack,
     TextField,
     Typography,
-  } from "@mui/material";
+    makeStyles
+} from "@mui/material";
 
-  export function Login() {
+
+export function Login() {
 
     return (
         <Container>
-            <Typography>The Cinelist</Typography>
-            <Paper elevation={1}>
-                <Stack direction={"column"} spacing={4}>
-                    <Typography>Faça o login</Typography>
-                    <TextField defaultValue={"Insira o email aqui"}></TextField>
-                    <TextField defaultValue={"Insira a senha aqui"}></TextField>
-                    <Button variant={"contained"}>
-                        <Typography >Fazer Login</Typography>
-                    </Button>
-                    <Typography>Não tem uma conta? Crie agora!</Typography>
-                    <Button variant={"contained"}>
-                        <Typography>Registrar uma conta</Typography>
-                    </Button>
-                </Stack>
-            </Paper>
+            <Box
+                sx={{ 
+                    height: "100vh",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-evenly"
+                }}
+            >
+                <Box>
+                    <Typography variant="h1">The CineList</Typography>
+                    <Paper elevation={1} 
+                        sx={{
+                            borderRadius: "28px",
+                            bgcolor: "rgba(18, 15, 51, 0.95)",
+                            color: "white"
+                        }}
+                    >
+                        <Stack direction={"column"} spacing={4}>
+                            <Typography variant="h4">Faça o login</Typography>
+                            <TextField 
+                                defaultValue={"Insira o email aqui"}
+                                sx={{ input: { color: 'white' }}}
+                            />
+                            <TextField 
+                                defaultValue={"Insira a senha aqui"}
+                                sx={{ input: { color: 'white' } }}
+                            />
+                            <Button variant={"contained"}>
+                                <Typography variant="s1">Fazer Login</Typography>
+                            </Button>
+                            <Typography>Não tem uma conta? Crie agora!</Typography>
+                            <Button variant={"contained"}>
+                                <Typography variant="s1">Registrar uma conta</Typography>
+                            </Button>
+                        </Stack>
+                    </Paper>
+                </Box>
+            </Box>
         </Container>
     );
-  }
+}
